9cd673d367af856f8f19c40acbce4a8160caac79
Danny Rooker <dannyrooks@gmail.com>
2019-07-11 18:51:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,9 +23,7 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = "Lagos"
-  my_favorite_cities.shift
+  array.shift
 end
 
 def shift_with_args(array)
